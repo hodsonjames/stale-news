a7c427de7a31a53a8d8de23bf94893cc1481000d
Richard Gong <richardgong@Richards-MacBook-Pro-4.local>
2019-02-15 15:10:48 -0800

    text file for role of functions

--- code/replication/utils.py
+++ code/replication/utils.py
@@ -2,8 +2,29 @@
 # -------
 # Helper functions for arguments to regression.py
 
+"""
+Functions that interface with measures directory in repo
+"""
+def abnormalPercentageOld(firm, date, file):
+    """
+        AbnPctOld for firm on given data
+        Uses decimal representation
+        Returns FLOAT
+    """
+
+def abnormalPercentageRecombinations(firm, date, file):
+    """
+        AbnPctRecombinations for firm on given data
+        Uses decimal representation
+        Returns FLOAT
+    """
+    
+"""
+Additional functions for regression
+"""
 def abnormalReturn(firm, date, file):
 	"""
+        #1
         Difference between firm i's return on date and return on value-weighted
         index of all firms in universe on date
         Uses decimal representation
@@ -14,6 +35,7 @@ def abnormalReturn(firm, date, file):
 
 def abnormalReturn(firm, dateStart, dateEnd, file):
 	"""
+        #2
         Cumulative abnormal returns for firm over [dateStart, dateEnd]
         Uses decimal representation
         Returns FLOAT
@@ -36,6 +58,7 @@ def allFirmsReturn(date, file):
 
 def abnormalVol(firm, date, file):
 	"""
+        #1
         Abnormal trading volume for firm on date defined as difference between the
         fraction of shares turned over for firm on date, and the value-weighted average 
         of the fraction of shares turned over for all firms in universe on date
@@ -46,6 +69,7 @@ def abnormalVol(firm, date, file):
 
 def abnormalVol(firm, dateStart, dateEnd, file):
 	"""
+        #2
         Average abnormal trading volume for firm over [dateStart, dateEnd]
         Uses decimal representation
         Returns FLOAT
@@ -89,6 +113,7 @@ def terms(firm, date, file):
 def marketCap(firm, date, file):
 	"""
         LOG market capitalization of firm as of market open on date
+        What kind of LOG?
         Returns FLOAT
     """
 
@@ -110,7 +135,21 @@ def illiquidity(firm, dateStart, dateEnd, file):
 	"""
 		LOG of the illiquidity measure from Amihud, computed as the prior-week average of 
 		10**6 * |Ret(firm,date)| / Volume(firm,date)
+        What kind of LOG?
 	"""
 
+def generateXVector(firm, data, file):
+    """
+        Creates 9 x 1 vector of controls for a firm including:
+            1. Storiesi,t
+            2. AbnStoriesi,[t-5,t-1]
+            3. Termsi,t
+            4. MCapi,t
+            5. BMi,t
+            6. AbnReti,[t-5,t-1]
+            7. AbnVoli,[t-5,t-1]
+            8. AbnVolitilityi,[t-5,t-1]
+            9. Illiqi,[t-5,t-1]
+    """
 
 
