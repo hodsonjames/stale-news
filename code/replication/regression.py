22483dacc9e150ad70d245278115d1cfbf4e62ec
Richard Gong <richardgong@Richards-MacBook-Pro-4.local>
2019-02-15 15:10:48 -0800

    text file for role of functions

--- code/replication/regression.py
+++ code/replication/regression.py
@@ -3,3 +3,15 @@
 # Functions for equations (8), (9), (10), (11), (12), (13) from Section 3
 
 from util import *
+
+"""
+Fama-MacBeth regression
+"""
+
+def famaMacBethRegression():
+
+"""
+Newey-West standard errors
+"""
+def neweyWestStdErrors():
+	
