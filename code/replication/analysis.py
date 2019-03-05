8487db6703fcdb0a58b77323633a9dc39e00d399
William Hsu <will.s.hsu@gmail.com>
2019-03-05 02:17:28 -0800

    Added Models

--- code/replication/analysis.py
+++ code/replication/analysis.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 from linearmodels.panel import FamaMacBeth # FamaMacBeth(dependent, exogenous)
 
-##### I. Import data #####
+# TODO: collect garbage (optimize memory)
+
+####### I. Import data #######
 headers = ("id", "ticker", "date", "time", "Old", "ClosestNeighbor", "length", "closest1", "closest2")
 data = pd.read_csv("../data/simulated_data.txt", names=headers)
 
@@ -12,7 +14,7 @@ def format_date(row):
 data["date"] = data.apply(format_date, axis=1)
 
 
-##### II. Construct individual story factors #####
+####### II. Construct individual story factors #######
 OLD_THRESHOLD = 0.6
 REPRINT_RECOMBINATION_THRESHOLD = 0.8
 data["OldNews"] = data["Old"] > OLD_THRESHOLD
@@ -22,7 +24,7 @@ data["Recombination"] = data.apply(lambda row: row["share_spanned"] < REPRINT_RE
 data["unique"] = data.apply(lambda row: (1 - row["Old"])*row["length"], axis=1) # TODO: make sure this is correct
 
 
-##### III. Construct firm factors #####
+####### III. Construct firm factors #######
 
 # pct factors
 dataGroup = data.groupby(['ticker', 'date'])
@@ -45,40 +47,59 @@ Construct abnormal firm factors (pg17):
     the log of the number of stories for ﬁrm i on date t, 
     the log of the average number of unique terms per story, and 
     the square of the log average number of unique terms per story
-pg17
-- (What was pctOld / pctRecombination for?)
-- This is also Fama MacBeth, right?
-- How to deal with 0 values?
 
-abnPctOld = extentOld - predicted extentOld
+This is also Fama-MacBeth, right?
 """
 firms["log(|S|)"] = np.log(firms[["|S|"]])
-firms["log(avg_unique)"] = np.log(firms[["avg_unique"]])
+firms["log(avg_unique)"] = np.log(firms[["avg_unique"]]) # ERROR: What do we do about 0 values?
 firms["log(avg_unique)^2"] = np.square(np.log(firms[["avg_unique"]]))
 
-# extentOldModel = FamaMacBeth(firms[["date", "ticker", "ExtentOld"]],  firms[["log(|S|)", "log(avg_unique)", "log(avg_unique)^2"]]).fit('heteroskedastic', 'bartlett')
-# print(extentOldModel)
-# extentOldPredictions = extentOldModel.predict(np.log(firms[["|S|"]]) + np.log(firms[["avg_unique"]]) + np.square(np.log(firms[["avg_unique"]])))
-# firms["abnPctOld"] = firms["ExtentOld"] - extentOldPredictions
+extentOldModel = FamaMacBeth(firms[["ExtentOld"]], firms[["log(|S|)", "log(avg_unique)", "log(avg_unique)^2"]]).fit('heteroskedastic', 'bartlett')
+firms["abnPctOld"] = firms[["ExtentOld"]] - extentOldModel.predict(firms[["log(|S|)", "log(avg_unique)", "log(avg_unique)^2"]])
+
+extentRecombinationModel = FamaMacBeth(firms[["ExtentRecombination"]], firms[["log(|S|)", "log(avg_unique)", "log(avg_unique)^2"]]).fit('heteroskedastic', 'bartlett')
+firms["abnPctRecombination"] = firms[["ExtentRecombination"]] - extentOldModel.predict(firms[["log(|S|)", "log(avg_unique)", "log(avg_unique)^2"]])
+
+
+####### IV. Regressions #######
+
+# TODO: Calculate full data, import (filtered to necessary) data
+# AbnRet[i,t+1]
+# AbnVol[i,t+1]
+# Controls:
 """
-firms["abnPctOld"] = firms.apply(lambda row: row["ExtentOld"] - extentOldModel.predict([np.log(row["|S|"]), 
-                                                                                        np.log(row["avg_unique"]), 
-                                                                                        np.square(np.log(row["avg_unique"]))]
-                                                                                        ), axis=1)
+Stories: |S| (relevance..? is that 1-old)
+AbnStories: calculated    # What does average stories mean? (per day?)
+Terms: avg_unique
+MCap: FROM DATA
+BM: FROM DATA
+AbnRet: calculated
+AbnVol: calculated
+Illiq: calculated
 """
-# extentRecombinationModel = FamaMacBeth().fit('heteroskedastic', 'bartlett')
+
+# Regressions for Market Reactions to Old News (pg18) 
+    # dependent: Abn___[i,t+1]
+    # exogenous: 1, AbnPctOld, Controls
+abnRetModel_OldNews = FamaMacBeth().fit('heteroskedastic', 'bartlett') # 8
+abnVolModel_OldNews = FamaMacBeth().fit('heteroskedastic', 'bartlett') # 9
+
+# Regressions for Market Reactions to Recombinations (pg20)
+    # dependent: Abn___[i,t+1]
+    # exogenous: 1, AbnPctOld, AbnPctRecombination, Controls
+abnRetModel_Recombination = FamaMacBeth().fit('heteroskedastic', 'bartlett') # 10
+abnVolModel_Recombination = FamaMacBeth().fit('heteroskedastic', 'bartlett') # 11
+
+# Regressions for Market Reversal Reactions to Recombinations (pg21)
+    # dependent: AbnRet[i,[t+t1,t+t2]]
+    # exogenous: 1, AbnPctOld, AbnPctOld*AbnRet, AbnRet, AbnPctRecombination, AbnPctRecombination*AbnRet, Controls
+abnRetModel_Reversal = FamaMacBeth().fit('heteroskedastic', 'bartlett') # 12
 
 
-##### IV. Regressions #####
+####### V. Output #######
 
-# Regression for old news vs. market reaction (8, 9)
-# nextDayAbnRetModel = FamaMacBeth(AbtRet, exog)
-
-# Regression for recombination vs. market reaction (10, 11)
-
-# Regression for reversal (12)
-
-
-##### V. Output #####
-
-print(firms)
+print(abnRetModel_OldNews)
+print(abnVolModel_OldNews)
+print(abnRetModel_Recombination)
+print(abnVolModel_Recombination)
+print(abnRetModel_Reversal)
