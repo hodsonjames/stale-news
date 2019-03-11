5cbb0b867e3442cb1a7d4fa1bad75be547abd6af
Richard Gong <richardgong@Richards-MacBook-Pro-4.local>
2019-03-11 16:55:56 -0700

    database fetch all implemented

--- code/replication/regression.py
+++ code/replication/regression.py
@@ -4,12 +4,20 @@
 
 import utils
 
-"""
-Fama-MacBeth regression
-"""
+def famaMacBethRegression(dateStart, dateEnd, dflist):
+    """
+    Average coefficients for daily cross-sectional regression over [dateStart, dateEnd]
+    dflist: list of data frames for each day
+    """
 
 
-def famaMacBethRegression():
+def generateFMDataFrame(XLists, AbnPctOldList, DependentList):
+    """
+    Returns pandas data frame organized for ols
+    XLists: list of XList corresponding to available firms on given day
+    AbnPctOldList: list of AbnPctOld corresponding to available firms on given day
+    DependentList: list of dependent variable corresponding to available firms on given day
+    """
 
 
 def neweyWestStdErrors():
