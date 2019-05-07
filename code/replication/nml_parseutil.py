8afa99e9fc1f38416859ac969666d82586697582
Rohan Narain <rohannarain@Rohans-MacBook-Pro-3.local>
2019-05-07 14:44:44 -0700

    code matches chris's output

--- code/replication/nml_parseutil.py
+++ code/replication/nml_parseutil.py
@@ -51,9 +51,7 @@ def filter_old_articles(company_article_map, curr_article, k_hours=sim.measure_c
     
     return company_article_set  
 
-# 
 
-############### CHANGE THE NAME OF THIS ######################
 def create_article_map(directory_path, output_csv_name, k_hours=sim.measure_const.NUM_HOURS):
     """
     Main parsing function. Takes in a directory containing .nml files
@@ -80,7 +78,10 @@ def create_article_map(directory_path, output_csv_name, k_hours=sim.measure_cons
 
     header_df = pd.DataFrame(columns=["company", "headline", "time", "id",
                                      "old_score", "closest_neighbor", "is_reprint", "is_recombination",
-                                     "num_characters"])
+                                     "num_unique_words"])
+    header_df = pd.DataFrame(columns=["DATE_EST", "STORY_ID", "TICKER", "HEADLINE",
+                                      "STORY_LENGTH", "CLOSEST_ID", "CLOSEST_SCORE", "TOTAL_OVERLAP",
+                                      "IS_OLD", "IS_REPRINT", "IS_RECOMBINATION"])
     header_df.to_csv(output_csv_name, index = False)
 
     f = open(output_csv_name, "a")
@@ -115,10 +116,10 @@ def create_article_map(directory_path, output_csv_name, k_hours=sim.measure_cons
                     all_text = xml_elem.find(".//text")
                     article_text = "".join(all_text.itertext())
                     text_stemmed_filtered = sim.stem_and_filter(article_text)
-                    num_characters = len(article_text)
+                    num_unique_words = len(text_stemmed_filtered)
 
                     for c in company:
-                        if c.attrib.get('about', False) != 'Y':
+                        if c.attrib.get('about', False) != 'Y' or "." in c.text:
                             curr_file_str = ""
                             continue
                         company = c.text
@@ -132,11 +133,10 @@ def create_article_map(directory_path, output_csv_name, k_hours=sim.measure_cons
                             curr_file_str = ""
                             continue
                         else:
-                            old, closest_neighbor = sim.compute_sim_measure(new_article, company_articles)
-                            new_row = [new_article.company, new_article.headline, new_article.timestamp, md5_hash,
-                                      old, closest_neighbor, sim.is_reprint(old, closest_neighbor),
-                                      sim.is_recombination(old, closest_neighbor),
-                                      num_characters]
+                            old, closest_neighbor, closest_id = sim.compute_sim_measure(new_article, company_articles)
+                            new_row = [new_article.timestamp, new_article.md5_id, new_article.company, new_article.headline,
+                                       num_unique_words, closest_id, closest_neighbor, old, sim.is_old_news(old),
+                                       sim.is_reprint(old, closest_neighbor), sim.is_recombination(old, closest_neighbor)]
                             csv_writer.writerow(new_row)
 
                             company_articles.add(new_article)
