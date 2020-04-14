3eb42c3d67dfb4e9e6307370ffc3c56e9618b3a9
Richard Gong <richardgong@Richards-MacBook-Pro-4.local>
2020-04-14 13:40:31 -0700

    update to MATLAB Fama-Macbeth example code

--- code/replication/matlab/example.m
+++ code/replication/matlab/example.m
@@ -1,4 +1,5 @@
 % Example run with test.csv for FamaMacBeth
-[y X PART_VAR] = FM_csv_args('test.csv', 12)
-BETA = zeros(10, 1);
-FamaMacBeth_NW(y, X, PART_VAR, BETA, 'NW', 1)
+[y, X, PART_VAR] = FM_csv_args('test.csv', 12);
+BETA = zeros(11, 1);
+LAG = double(uint64(0.75 * length(unique(PART_VAR)) ^ (1/3)));
+RET = FamaMacBeth_NW(y, X, PART_VAR, BETA, 'NW', LAG);
